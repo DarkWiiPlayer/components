b02ce36c932e85029bdd0ba09c14169023ec6b29
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2024-08-27 10:33:58 +0200

    Further refactor color scheme switcher

--- ColorSchemeSwitcher.js
+++ ColorSchemeSwitcher.js
@@ -1,4 +1,24 @@
+const styles = `
+	light-mode-only, dark-mode-only { display: none; }
+	:root[color-scheme="dark"] dark-mode-only { display: initial; }
+	@media (prefers-color-scheme: dark) {
+		:root:not([color-scheme]) dark-mode-only { display: initial; }
+	}
+	:root[color-scheme="light"] light-mode-only { display: initial; }
+	@media (prefers-color-scheme: light) {
+		:root:not([color-scheme]) light-mode-only { display: initial; }
+	}
+	:root:not([color-scheme]) override-scheme-only { display: none }
+	:root[color-scheme] automatic-scheme-only { display: none }
+`
+
 export default class extends HTMLElement {
+	static installHelperStyles() {
+		const style = document.createElement("style")
+		style.innerHTML = styles
+		document.head.append(style)
+	}
+
 	connectedCallback() {
 		if (this.stored) {
 			this.set(this.stored)
@@ -16,23 +36,42 @@ export default class extends HTMLElement {
 		if (root.hasAttribute("color-scheme"))
 			return root.getAttribute("color-scheme")
 		else
-			return window.matchMedia('(prefers-color-scheme: dark)') ? "dark" : "light"
+			return window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
+	}
+
+	get automatic() {
+		const root = document.documentElement
+		return !root.hasAttribute("color-scheme")
 	}
 
 	toggle() {
-		const scheme = (this.current == "dark") ? "light" : "dark"
-		this.set(scheme)
+		this.set((this.current == "dark") ? "light" : "dark")
+	}
+
+	triToggle() {
+		if (this.automatic)
+			this.set("light")
+		else if (this.current == "light")
+			this.set("dark")
+		else
+			this.reset()
+	}
+
+	reset() {
+		localStorage?.removeItem?.("color-scheme")
+		document.documentElement.removeAttribute("color-scheme")
+		this.setCurrent()
 	}
 
 	/** @param {string} scheme */
 	set(scheme) {
 		const root = document.documentElement
 		root.setAttribute("color-scheme", scheme)
-		localStorage?.setItem("color-scheme", scheme)
+		localStorage?.setItem?.("color-scheme", scheme)
 		this.setCurrent()
 	}
 
 	get stored() {
-		return localStorage?.getItem("color-scheme")
+		return localStorage?.getItem?.("color-scheme")
 	}
 }
